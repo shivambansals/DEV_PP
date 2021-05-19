e75d851f83eeb3a16d2b2e7225c18f3f1f326ee4
shivambansals <shivam.bansal_iot18@gla.ac.in>
2021-05-19 14:57:11 +0530

    JS Concept 1

---
+++ Module1/JSConcepts/fun.js
@@ -0,0 +1,12 @@
+console.log(a);
+console.log(fun);
+fun();
+
+
+var a = 20;
+var fun = function(){
+    console.log("Fun says hi !!!");
+}
+
+console.log(fun);
+console.log(a);
---
+++ Module1/JSConcepts/hoisting.js
@@ -0,0 +1,18 @@
+// console.log(a);
+// var a = "steve";
+// console.log(a);
+
+
+
+
+
+
+//console.log(a);
+
+
+
+
+
+console.log(b);
+let b = "steve";
+console.log(b);
