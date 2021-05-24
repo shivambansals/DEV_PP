a1e417be62a9664e939744141e8e999d9a37361c
shivambansals <shivam.bansal_iot18@gla.ac.in>
2021-05-24 19:21:13 +0530

    login hackerrank

--- Module1/Lec8_Automation/login.js
+++ Module1/Lec8_Automation/login.js
@@ -1,14 +1,26 @@
 const puppeteer = require('puppeteer');
-let browserOpenPromise = puppeteer.launch({headless: false});
+const id = "niyeja8584@sc2hub.com"
+const pw = "123456";
+let tab;
+let browserOpenPromise = puppeteer.launch({headless: false, defaultViewport : null , args : ["--start-maximized"]});
 //console.log(browserOpenPromise);
 browserOpenPromise.then(function(browser){
     console.log("browser is opened !");
-    return abc.pages();
+    return browser.pages();
 })
-.then(function(browser){
-    let tab = pages[0];
-    return tab.goto("http://www.google.com");
+.then(function(pages){
+     tab = pages[0];
+    return tab.goto("https://www.hackerrank.com/auth/login");
 })
 .then(function(){
-    console.log("On google homepage !!");
+    return tab.type("#input-1" , id);
+})
+.then(function(){
+    return tab.type("#input-2" , pw);
+})
+.then(function(){
+    return tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+})
+.then(function(){
+    console.log("logged in !!!")
 })
