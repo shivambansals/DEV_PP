c936db8abc00272091594d1dce12d2a061d40a5a
shivambansals <shivam.bansal_iot18@gla.ac.in>
2021-05-24 17:19:43 +0530

    scope

---
+++ Module1/JSConcepts/scope.js
@@ -0,0 +1,38 @@
+// var a = 20;
+// console.log(a);
+
+// if(true){
+//   // var a =50;
+//    console.log(a);
+// }
+
+// function callMe(){
+//     console.log(a);
+//     //var a = 100;
+//     console.log("Inside call me");
+//     console.log(a);
+// }
+
+// callMe();
+
+
+
+
+let a = 20;
+console.log(a);
+
+if(true){
+   let a =50;
+   console.log(a);
+}
+
+function callMe(){
+    console.log("iNSIDE CALL ME");
+    console.log(a);
+   let a = 100;
+    console.log(a);
+    
+}
+
+callMe();
+
