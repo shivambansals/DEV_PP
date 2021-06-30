8ab661fbba00cc8ceb9fa6454ccbb9b0054aa5fe
shivambansals <shivam.bansal_iot18@gla.ac.in>
2021-06-30 16:06:02 +0530

    db initialized

--- Module2/ExcelClone/init.js
+++ Module2/ExcelClone/init.js
@@ -26,4 +26,23 @@ function initCells(){
     cellsContent += "</div>";
     cellsContainer.innerHTML = cellsContent;
 }
+
+function initDB(){
+    db = [];
+    for(let i=0; i<100; i++){
+        let row = [];
+        for(let j=0; j<26; j++){
+            let cellName = String.fromCharCode(65+j) + (i+1);
+            let cellObject = {
+                name : cellName,
+                value :"",
+                formula:"",
+            };
+            row.push(cellObject);
+        }
+        db.push(row);
+    }
+    console.log(db);
+}
 initCells();
+initDB();
